de1a73eff9789383308c44583f8f8ca95a184544
cyber <cyber1038@gmail.com>
2025-04-11 16:44:26 +0300

    feat: am facut functie pentru task1

--- GameofLife.c
+++ GameofLife.c
@@ -26,49 +26,27 @@ int neighbours(const char mat[][101], int i, int j, int linii, int coloane)  ///
     }
     return nrX;
 }
-
-int main(int argc, const char* argv[])
+void task1(int n, int m, int k, char matrice[][101], FILE* output)
 {
-    for(int t = 1; t < argc; t+=2)
-    {
-        FILE *input_file = fopen(argv[t], "r");
-        FILE *output_file = fopen(argv[t+1], "w");
-
-        verifis(input_file);
-        verifis(output_file);
-
-        char GoF[101][101];
-        int T,N,M,K;
-
-        fscanf(input_file,"%d",&T);     ///citire numar task
-
-        if(t<=5 && T==1)  ///task 1
-        {
-            fscanf(input_file, "%d %d %d", &N, &M, &K);  
-
-            for(int i=0;i<N;i++)                                    ///citire data*.in unde * numar de la 1 la 5
-                for(int j=0;j<M;j++)                            
-                    fscanf(input_file, " %c", &GoF[i][j]);
- 
-            for(int i=0;i<N;i++)  ///afisam matricea initiala
+    for(int i=0;i<n;i++)  ///afisam matricea initiala
             {
-                for(int j=0;j<M;j++)
-                    fprintf(output_file,"%c", GoF[i][j]);       ///in data*.out
-                fprintf(output_file,"\n");
+                for(int j=0;j<m;j++)
+                    fprintf(output,"%c", matrice[i][j]);       ///in data*.out
+                fprintf(output,"\n");
             }
-            fprintf(output_file,"\n");
+            fprintf(output,"\n");
 
-            while(K>0)
+            while(k>0)
             {
                 char newGoF[101][101];  ///aici o sa punem modificarile pentru a respecta exact regulile jocului
             
-                for(int i=0;i<N;i++)
+                for(int i=0;i<n;i++)
                 {   
-                    for(int j=0;j<M;j++)
+                    for(int j=0;j<m;j++)
                     {
-                        int vecini=neighbours(GoF, i,j, N,M);
+                        int vecini=neighbours(matrice, i,j, n,m);
 
-                        if(GoF[i][j]=='X')
+                        if(matrice[i][j]=='X')
                         {
                             if(vecini<2 || vecini>3)  ///subpopulare && suprapopulare
                                 newGoF[i][j]='+';
@@ -86,21 +64,47 @@ int main(int argc, const char* argv[])
                     }
                 }
 
-                for(int i=0;i<N;i++)  ///copiem matricea temporara in matricea noastra
-                    for(int j=0;j<M;j++)
-                        GoF[i][j]=newGoF[i][j];
+                for(int i=0;i<n;i++)  ///copiem matricea temporara in matricea noastra
+                    for(int j=0;j<m;j++)
+                        matrice[i][j]=newGoF[i][j];
 
-                for(int i=0;i<N;i++)  ///afisam matricea de urmatoarea generatie
+                for(int i=0;i<n;i++)  ///afisam matricea de urmatoarea generatie
                 {
-                    for(int j=0;j<M;j++)
-                        fprintf(output_file,"%c", GoF[i][j]);       ///in data*.out
-                    fprintf(output_file,"\n");
+                    for(int j=0;j<m;j++)
+                        fprintf(output,"%c", matrice[i][j]);       ///in data*.out
+                    fprintf(output,"\n");
                 }
-                fprintf(output_file,"\n");
+                fprintf(output,"\n");
 
-                K--;
+                k--;
             }
-        }
+}
+
+int main(int argc, const char* argv[])
+{
+    for(int t = 1; t < argc; t+=2)
+    {
+        FILE *input_file = fopen(argv[t], "r");
+        FILE *output_file = fopen(argv[t+1], "w");
+
+        verifis(input_file);
+        verifis(output_file);
+
+        char GoF[101][101];
+        int T,N,M,K;
+
+        fscanf(input_file,"%d",&T);     ///citire numar task
+
+        fscanf(input_file, "%d %d %d", &N, &M, &K);  
+
+            for(int i=0;i<N;i++)                                    ///citire data*.in unde * numar de la 1 la 5
+                for(int j=0;j<M;j++)                            
+                    fscanf(input_file, " %c", &GoF[i][j]);
+        
+        if(t<=5 && T==1)  ///task 1
+            task1(N,M,K,GoF,output_file);
+        else if(t>=6 && t<=10 && T==2 )
+
         fclose(input_file);
         fclose(output_file);
     }
